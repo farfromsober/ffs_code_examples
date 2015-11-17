5843a11cbbc7a1ce208a6818d66b253d887f0bbf
Joan Biscarri <joan@biscarri.com>
2015-11-18 00:07:04 +0100

    Initial Commit

---
+++ AzureExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AzureExample
+//
+//  Created by Joan on 18/11/15.
+//  Copyright © 2015 biscarri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AzureExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AzureExample
+//
+//  Created by Joan on 18/11/15.
+//  Copyright © 2015 biscarri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
